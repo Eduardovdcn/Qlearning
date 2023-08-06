292c38db8fb937c42c47cf931a8e8acd12663be3
Eduardovdcn <72761294+Eduardovdcn@users.noreply.github.com>
2023-08-06 19:07:37 -0300

    prototipo final

--- client.py
+++ client.py
@@ -13,13 +13,25 @@ def get_movement(move: int) -> str:
     2: jump
     """
 
-    if 0 <= move or move >= 3:
+    if 0 > move or move >= 3:
         raise ValueError(
             'Invalid move value. Only values between 0 and 2 are allowed (inclusive)'
         )
 
     print('next movement: ', constants.MOVE_MAP[move])
     return constants.MOVE_MAP[move]
+
+
+def get_next_direction(matriz: str, plataforma: int): 
+    biggest = -9999
+    direction = -1
+
+    for x in range(3):
+        if matriz[x][plataforma] >= biggest: #tem que garantir que as duas variáveis são do mesmo tipo!
+            biggest = matriz[x][plataforma]
+            direction = x
+
+    return direction
 
 
 def get_next_platform(state: str) -> int:
@@ -46,6 +58,7 @@ def write_policy_file(df: DataFrame):
 
 
 def update_util(
+    matriz,
     curr_direction,
     curr_platarform,
     next_direction,
@@ -62,20 +75,22 @@ def update_util(
 
 
 def main():
-    const_alfa = 0.9
-    const_gama = 0.5
-
     connect = con.connect(2037)
-    state = 1010000
+    state = '1b1010000'
+    curr_platarform = int(state[2:7], 2)
+    curr_direction = int(state[7:9], 2)
+    print(curr_direction)
 
-    for _ in range(50):
-        curr_platarform = state[0:6]
-        curr_direction = state[6:8]
+    matriz = load_policy_from_file('resultado.txt')
 
-        act = define_act(curr_direction)
+    for _ in range(constants.LOOP_ITERATIONS):
+        print(curr_direction)
+        act = get_movement(curr_direction)
         state, reward = con.get_state_reward(connect, act)
 
-        next_plataform, next_direction = get_next_act()
+        next_plataform = get_next_platform(state)
+
+        next_direction = get_next_direction(matriz, next_plataform)
 
         matriz[curr_direction][curr_platarform] = update_util(
             matriz,
@@ -84,15 +99,17 @@ def main():
             next_direction,
             next_plataform,
             reward,
-            const_alfa,
-            const_gama,
+            constants.ALPHA,
+            constants.GAMMA,
         )
         curr_direction = next_direction
         curr_platarform = next_plataform
 
-        if reward == 300:
-            pass
+        if reward == 300:   break
 
+    write_policy_file(matriz)
+
+    print('acabou :(')
 
 if __name__ == '__main__':
     main()
